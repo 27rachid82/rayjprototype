814f41abea9e12e3277984c31b184b04709292c1
27rachiid82 <mohammedikacirachid@yahoo.fr>
2023-12-12 19:58:08 +0100

    validate structure page

--- src/components/Display_offer.jsx
+++ src/components/Display_offer.jsx
@@ -1,9 +1,33 @@
+import "/src/css/display_offer.css";
 
 export default function Display_offer() {
     return (
         <>
-        <h3>Description de l&apos;offre</h3> 
-            <p>Vous aurez la responsabilité de....</p>       
+        <h2>Développeur Web</h2>
+
+            <hr className="trait"></hr>
+            <section className="cadre">
+               <h3>Description de l&apos;offre</h3> 
+                    <p>Vous aurez la responsabilité de blabla....
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum molestias ab soluta voluptas ullam? Nihil ullam deleniti libero voluptas possimus sit, repellat quaerat! Recusandae enim rerum, ipsam quam perspiciatis non!</p>   
+            <hr className="trait"></hr>  
+
+                <h3>Compétences recherchées</h3>
+
+                <div className="competences-container">
+                    <ul className="competences-column">
+                        <li>✓ Excell</li>
+                        <li>✓ Autonomie</li>
+                    </ul>
+                    <ul className="competences-column">
+                        <li>✓ Réactivité</li>
+                        <li>✓ Word</li>
+                    </ul>
+                </div>
+
+
+            </section>
+        <button className="buttoncandidate"><p className="textbutton">CANDIDATER</p></button>
         </>
     )
 }
--- src/pages/Description_offre.jsx
+++ src/pages/Description_offre.jsx
@@ -4,7 +4,8 @@ import Display_offer from "/src/components/Display_offer.jsx";
  export default function Description_offre() {
     return (
         <>
-        <h1>WELCOME TO PROJECT 3 !</h1>
+        <h1>WELCOME TO PROJECT RAYJ !</h1>
+       
             <Display_offer />
         </>
     )
